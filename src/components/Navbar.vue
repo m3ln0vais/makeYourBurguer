adfc8ef1b7d1a2c6aea8029b0fe2a298b08c4d5d
m3ln0vais <mellnovais14@gmail.com>
2023-07-26 02:20:39 -0300

    teste

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <router-link to="/">
+      <img src="" alt="" id="logo" />
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedidos">About</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+};
+</script>
